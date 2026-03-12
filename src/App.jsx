a924ac047ce0820047aa0b99d9f214c435435bb8
Krishna Chaturvedi <krishnachaturvedi@Krishnas-MacBook-Air.local>
2026-03-12 22:22:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StolenTimeline from './Timeline';
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
+      <StolenTimeline />
+    </div>
+  );
+}
+
+export default App;
